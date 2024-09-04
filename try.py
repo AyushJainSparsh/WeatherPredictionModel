3d1af66a4d57cc8d47afb1cb50b9303de7ad2edd
Ayush Jain <155948387+AyushJainSparsh@users.noreply.github.com>
2024-09-04 21:17:10 +0530

    Delete try.py

--- try.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-
-model1 = pkl.load(open('encoder_condition.pkl' , 'rb'))
-model2 = pkl.load(open('condition_model.pkl' , 'rb'))
-
-output = model2.predict([[23.8,4.7,1004.0,41.0,24.8,24.8]])
-print(model1.inverse_transform([output]))
-
-print(model1.inverse_transform([2]))
